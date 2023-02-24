224b33cbff031d2db231f2b5bfd7bdf39642fe3a
wuying <106313976+612901@users.noreply.github.com>
2023-02-24 19:14:36 +0800

    Update nav.js

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,21 +1,17 @@
-// 返回顶部 显示网页阅读进度
-window.onscroll = percent; // 执行函数
+window.onscroll = percent;// 执行函数
 // 页面百分比
 function percent() {
-  let a = document.documentElement.scrollTop || window.pageYOffset, // 卷去高度
-    b =
-      Math.max(
-        document.body.scrollHeight,
-        document.documentElement.scrollHeight,
-        document.body.offsetHeight,
-        document.documentElement.offsetHeight,
-        document.body.clientHeight,
-        document.documentElement.clientHeight
-      ) - document.documentElement.clientHeight, // 整个网页高度 减去 可视高度
-    result = Math.round((a / b) * 100), // 计算百分比
-    btn = document.querySelector("#percent"); // 获取图标
+    let a = document.documentElement.scrollTop || window.pageYOffset, // 卷去高度
+        b = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.body.clientHeight, document.documentElement.clientHeight) - document.documentElement.clientHeight, // 整个网页高度
+        result = Math.round(a / b * 100), // 计算百分比
+        up = document.querySelector("#go-up") // 获取按钮
 
-  result <= 99 || (result = 99), (btn.innerHTML = result);
+    if (result <= 95) {
+        up.childNodes[0].style.display = 'none'
+        up.childNodes[1].style.display = 'block'
+        up.childNodes[1].childNodes[0].innerHTML = result;
+    } else {
+        up.childNodes[1].style.display = 'none'
+        up.childNodes[0].style.display = 'block'
+    }
 }
-
-document.getElementById("page-name").innerText = document.title.split(" | 安知鱼")[0];
