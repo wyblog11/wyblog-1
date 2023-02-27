e2e27228ce67d09a6dc5f4d2e9fa672b051bb2e6
wuying <106313976+612901@users.noreply.github.com>
2023-02-27 21:49:45 +0800

    Update bb.js

--- source/ispeak/bb.js
+++ source/ispeak/bb.js
@@ -1,119 +1,59 @@
-let jsonUrl = 'https://kkapi.wyblog1.tk/api/ispeak?author=63c28a71aa610fa0dc9b6f1a' // 在这修改api
+<script>
+let svg = '<svg  viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" class="is-badge"><path  d="m512 268c0 17.9-4.3 34.5-12.9 49.7s-20.1 27.1-34.6 35.4c.4 2.7.6 6.9.6 12.6 0 27.1-9.1 50.1-27.1 69.1-18.1 19.1-39.9 28.6-65.4 28.6-11.4 0-22.3-2.1-32.6-6.3-8 16.4-19.5 29.6-34.6 39.7-15 10.2-31.5 15.2-49.4 15.2-18.3 0-34.9-4.9-49.7-14.9-14.9-9.9-26.3-23.2-34.3-40-10.3 4.2-21.1 6.3-32.6 6.3-25.5 0-47.4-9.5-65.7-28.6-18.3-19-27.4-42.1-27.4-69.1 0-3 .4-7.2 1.1-12.6-14.5-8.4-26-20.2-34.6-35.4-8.5-15.2-12.8-31.8-12.8-49.7 0-19 4.8-36.5 14.3-52.3s22.3-27.5 38.3-35.1c-4.2-11.4-6.3-22.9-6.3-34.3 0-27 9.1-50.1 27.4-69.1s40.2-28.6 65.7-28.6c11.4 0 22.3 2.1 32.6 6.3 8-16.4 19.5-29.6 34.6-39.7 15-10.1 31.5-15.2 49.4-15.2s34.4 5.1 49.4 15.1c15 10.1 26.6 23.3 34.6 39.7 10.3-4.2 21.1-6.3 32.6-6.3 25.5 0 47.3 9.5 65.4 28.6s27.1 42.1 27.1 69.1c0 12.6-1.9 24-5.7 34.3 16 7.6 28.8 19.3 38.3 35.1 9.5 15.9 14.3 33.4 14.3 52.4zm-266.9 77.1 105.7-158.3c2.7-4.2 3.5-8.8 2.6-13.7-1-4.9-3.5-8.8-7.7-11.4-4.2-2.7-8.8-3.6-13.7-2.9-5 .8-9 3.2-12 7.4l-93.1 140-42.9-42.8c-3.8-3.8-8.2-5.6-13.1-5.4-5 .2-9.3 2-13.1 5.4-3.4 3.4-5.1 7.7-5.1 12.9 0 5.1 1.7 9.4 5.1 12.9l58.9 58.9 2.9 2.3c3.4 2.3 6.9 3.4 10.3 3.4 6.7-.1 11.8-2.9 15.2-8.7z" fill="#1da1f2"></path></svg>'
+let total = 0
+let nowNum = 0
+let items = []
+let page = 1
+let Url = 'https://kkapi.wyblog1.tk/api/ispeak?author=63c28a71aa610fa0dc9b6f1a' // 修改api，记得带参数page
 
-document.getElementById('bber-talk').addEventListener('click', () => {
-    window.location.pathname = '/ispeak/' // 在这修改你的哔哔页面地址
-})
 
-bbtalk();
-
-function bbtalk() {
-    let data = JSON.parse(localStorage.getItem('bibi'));
-    let nowTime = Date.now();
-    let ls;
-    if (data == null || nowTime - data.time >= 1800000) { // 设置缓存时长，单位毫秒，默认30分钟，建议10分钟以上，不能为0，想不缓存自己改代码。
-        getData();
-        return
-    } else {
-        ls = JSON.parse(data.ls)
-    };
-    let bberHtml = ''
-    ls.forEach((item, i) => {
-        let br = /[\s\uFEFF\xA0]+/g;
-        item.content = item.content.replace(br, '')
-        let d = new Date(item.createdAt)
-        let date = d.getFullYear() + '/' + (d.getMonth() + 1) + '/' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds()
-        let dataTime = timeago.format(date, 'zh_CN');
-        let newdataTime = '<span class="datatime">' + dataTime + '</span>'
-
-        bberHtml += '<li class="item item-' + (i + 1) + '">' + newdataTime + '： ' + urlToLink(item.content) + '</li>'
-    });
-    document.getElementById("bber-talk").innerHTML += '<i style="margin-right: 10px;" class="fa-regular fa-message"></i><ul class="talk-list">' + bberHtml + '</ul><i class="fa-solid fa-angles-right pass bber-icon"></i>'
-}
-
-function getData() {
-    fetch(jsonUrl)
-        .then(res => res.json())
-        .then((data) => {
-            data = { time: Date.now(), ls: JSON.stringify(data.data.items) }
-            localStorage.setItem('bibi', JSON.stringify(data))
-        }).then(() => {
-            bbtalk();
-        }).catch(() => {
-            console.log('获取哔哔数据失败！');
-        });
-}
-
-function urlToLink(str) {
-    let re_forimg = /<img(.*?)src=[\"|\']?(.*?)[\"|\']?(.*?)>|!\[(.*?)\]\((.*?)\)/g;
-    str = str.replace(re_forimg, '<i class="fa-solid fa-image"></i>');
-    return str
-}
-
-function Roll() {
-    try {
-        let list_li = Array.prototype.slice.call(document.querySelectorAll('.talk-list li'));
-        let tmp = list_li[0];
-        list_li.splice(0, 1);
-        list_li.push(tmp);
-        let list = document.querySelector('ul.talk-list')
-        list_li.forEach((item) => {
-            list.appendChild(item)
-        });
-    } catch (error) {}
-};
-setInterval(Roll, 3000);
-
+window.addEventListener('DOMContentLoaded', () => {
+    getNew();
+});
 
 // 获取数据
 function getNew() {
-    fetch(Url).then(res => res.json()).then((res) => {
+    let bibi = document.getElementById('bibi');
+    try {
+        bibi.removeChild(document.getElementById('more'))
+    } catch (error) { }
+
+    bibi.innerHTML += '<div id="bb_loading"><img src="/assets/loading3.gif" alt="bb_loading"></div>' // bb_loading图片可以f12在我网站源码下载，也可以使用其他图片。
+
+    fetch(Url + page).then(res => res.json()).then((res) => {
+        total = res.data.total
         items = res.data.items
+        nowNum += items.length
+        if (page == 1) {
+            document.querySelector('.bb-info').innerHTML = '<svg style="width:1.20em;height:1.20em;top:5px;fill:currentColor;overflow:hidden;position:relative"><use xlink:href="#icon-chat"></svg> 站长的唠叨(' + total + ')'
+        }
+        page += 1
     }).then(() => {
         bb();
+        if (nowNum < total) {
+            document.getElementById('bibi').innerHTML += '<button id="more" onclick="getNew()">再翻翻</button>'
+        }
+        document.getElementById('bibi').removeChild(document.getElementById('bb_loading'))
     })
 }
 
 // 渲染数据
 function bb() {
-    let bb = document.getElementById('bibi')
-    if (items.length == 30) {
-        document.querySelector('.limit').style.display = 'block'
-    }
-
+    let bb = document.getElementById('bb-main')
     items.forEach((item) => {
-        let d = new Date(item.createdAt)
-        let date = d.getFullYear() + '/' + (d.getMonth() + 1) + '/' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds()
-        let dataTime = timeago.format(date, 'zh_CN');
+        let time = item.createdAt.substring(0, 10);
+        let div = document.createElement('div')
+        item.content = contentFormat(item.content)
 
-        bb.innerHTML += '<div class="bb-box"><div class="bb-content">' + contentFormat(item.content) + '</div><div class="bb-bottom"><span class="time">' + dataTime + '</span> <span style="margin-left:5px;"><i class="fa-solid fa-tag"></i> ' + item.tag.name
+        div.className = 'bb-card'
+        div.innerHTML = '<div class="card-header"><div class="avatar"><img class="nofancybox"src="' + item.author.avatar + '"></div><div class="name">' + item.author.nickName + '</div>' + svg + '<div class="card-time">' + time + '</div></div><div class="card-content">' + item.content + '</div><div class="card-footer"><div data-v-185689ea=""class="card-label"style="background: ' + item.tag.bgColor + '; color: white;">' + item.tag.name + '</div></div>'
+        bb.appendChild(div)
     })
-    var x = document.getElementById("bbcontainer");
-    if (x != null)
-    x.remove();
-}
-
-function contentToText(s) {
-    let br = /<\/*br>|[\s\uFEFF\xA0]+/g;
-    let re_forimg = /<img(.*?)src=[\"|\']?(.*?)[\"|\']?(.*?)>|!\[(.*?)\]\((.*?)\)/g;
-    let getImgUrl = /(http(.*).[jpg|png|gif])/g;
-    let ls = s.match(getImgUrl)
-    s = s.replace(re_forimg, '')
-    s = s.replaceAll(br, '')
-
-    let text = ''
-    if (ls) {
-        ls.forEach((e) => {
-            text += '[图片]'
-        })
-    }
-    s += text
-    console.log(s);
-    return s
 }
 
 // content格式化
 function contentFormat(s) {
-    let br = /<\/*br>|^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+    let br = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
     let re_forimg = /<img(.*?)src=[\"|\']?(.*?)[\"|\']?(.*?)>|!\[(.*?)\]\((.*?)\)/g;
     let getImgUrl = /(http(.*).[jpg|png|gif])/g;
     let ls = s.match(getImgUrl)
@@ -129,4 +69,3 @@ function contentFormat(s) {
     s += html
     return s
 }
-getNew();
