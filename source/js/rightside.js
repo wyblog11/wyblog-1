4ba49c3e01e8508043c21ad18768c0f790ce2754
wuying <106313976+612901@users.noreply.github.com>
2023-03-03 22:20:40 +0800

    Delete rightside.js

--- source/js/rightside.js
+++
@@ -1,7 +0,0 @@
-var rightside = {
-    // RightSide设置
-    switchRightSide: function () {
-        document.getElementById('rightside')?.classList.toggle('hidden')
-        document.getElementById('rightside-mask')?.classList.toggle('hidden')
-    },
-}
