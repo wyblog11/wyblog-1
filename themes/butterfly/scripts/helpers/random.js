754067d48fdbc27cf2965390962302fd4b3ca7a7
wuying <106313976+612901@users.noreply.github.com>
2023-03-10 21:03:33 +0800

    Update random.js

--- themes/butterfly/scripts/helpers/random.js
+++ themes/butterfly/scripts/helpers/random.js
@@ -9,19 +9,3 @@ hexo.extend.generator.register('random', function (locals) {
         data: `var posts=${JSON.stringify(posts)};function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};`
     }
 })
-
-/**
- * 随机友链
- */
-hexo.extend.filter.register('after_render:html', function (data) {
-    const flinks = []
-    // 获取所有友链链接
-    hexo.locals.get('data').link.map(function (list) {
-        list.link_list.map(function (flink) {
-            flinks.push(flink.link)
-        })
-    })
-    // 随机获取一个友链
-    data += `<script>var flinks=${JSON.stringify(flinks)};function toRandomFlink(){window.open(flinks[Math.floor(Math.random()*flinks.length)]);};</script>`
-    return data
-})
