a654c30f0fd8a72edb68df672ebf7800410cd315
wuying <106313976+612901@users.noreply.github.com>
2023-04-29 21:09:00 +0800

    Update wyblog.js

--- themes/butterfly/source/js/wyblog.js
+++ themes/butterfly/source/js/wyblog.js
@@ -1,3 +1,22 @@
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+  if (document.hidden) {
+    //离开当前页面时标签显示内容
+    document.title = '👀跑哪里去了~';
+    clearTimeout(titleTime);
+  } else {
+    //返回当前页面时标签显示内容
+    document.title = '🐖抓到你啦～';
+    //两秒后变回正常标题
+    titleTime = setTimeout(function () {
+      document.title = OriginTitile;
+    }, 2000);
+  }
+});
+
+
 //分类条
 function categoriesBarActive() {
 	var urlinfo = window.location.pathname;
